02023c381e1f876e570e1f24ef2bba9bd5c2dc61
joycehsu <m234onica@gmail.com>
2020-04-30 17:08:48 +0800

    templated ,pull text to json and add favicon

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,38 @@
 var clean = require("gulp-clean"),
     gulp = require("gulp"),
     gulpImagemin = require("gulp-imagemin"),
-    data = require("gulp-data");
+    data = require("gulp-data"),
+    fs = require("fs"),
+    path = require("path");
 
 gulp.task("clean", function () {
     return gulp.src("./docs")
         .pipe(clean());
 })
-gulp.task("CNAME", function() {
+gulp.task("CNAME", function () {
     return gulp.src("./CNAME")
         .pipe(gulp.dest("./docs/"));
 })
 
-gulp.task("HTML", function () {
-    return gulp.src("./*.html")
-        .pipe(gulp.dest("./docs/"));
+gulp.task('compile', function () {
+    "use strict";
+    var twig = require("gulp-twig");
+    return gulp.src("./index.html")
+        .pipe(data(
+            function (file) {
+                data = JSON.parse(fs.readFileSync("./data/index.html.json"));
+                return { words: data.words }
+            }
+        ))
+        .pipe(twig({
+            title: "Gulp and Twig",
+            benefits: [
+                'Fast',
+                'Flexible',
+                'Secure'
+            ]
+        }))
+        .pipe(gulp.dest('./docs/'));
 })
 
 gulp.task("CSS", function () {
@@ -28,6 +46,6 @@ gulp.task("IMGAGE", function () {
         .pipe(gulp.dest("./docs/img/"));
 })
 
-gulp.task("default", gulp.series("clean", "CNAME", "HTML", "CSS", "IMGAGE"), function (done) {
+gulp.task("default", gulp.series("clean", "CNAME", "compile", "CSS", "IMGAGE"), function (done) {
     done();
 });
